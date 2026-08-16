c276b0400221d43554921d961fa5707e27fbb990
Kareem Ashraf <kareemashrafsaber7@gmail.com>
2026-08-16 10:26:12 +0300

    Update dim_vehicle.sql

--- lambdaBatchSG/models/gold/dim_vehicle.sql
+++ lambdaBatchSG/models/gold/dim_vehicle.sql
@@ -10,7 +10,7 @@ SELECT
     vehicle_model,
     vehicle_color,
     license_plate,
-    event_timestamp as vehicle_updated_at,
+    vehicle_updated_at,
     dbt_valid_from as valid_from,
     dbt_valid_to as valid_to
 FROM
